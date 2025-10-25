e46dbaeb612eac2cff2e9298fe28745d75c35515
cinglein <ietgas@gmail.com>
2025-10-25 19:49:27 +0000

    change to LruCache

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Temporal Takehome
+
+## Incipit
+
+This program tracks SolanaMEVBot `MEViEnscUm6tsQRoGd9h6nLQaQspKj7DB2M5FwM3Xvz` trades to look at its profitability.
+
+The database is defined in the `migrations` folder. I write everything to a `txs` table that tracks:
+
+- Feepayer wallet
+- TX signature
+- Blocktime
+- Slot
+- Fee
+- Raw arb profit
+
+The program is currently deployed to a EC2 box and writes to a NeonDB database. There are three Grafana views which track net profitability per trade, trades per hour, and net profitability per hour.
--- src/time.rs
+++ src/time.rs
@@ -1,23 +1,26 @@
 use crate::util::Config;
+use core::num::NonZeroUsize;
+use lru::LruCache;
 use solana_client::nonblocking::rpc_client::RpcClient;
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 use tokio::{sync::Mutex, time::*};
 
 const SLEEP_INTERVAL: Duration = Duration::from_millis(400);
+const MAX_SLOTS: NonZeroUsize = NonZeroUsize::new(100).unwrap();
 
 pub struct SlotMap {
     client: RpcClient,
-    slots: HashMap<u64, i64>,
+    slots: LruCache<u64, i64>,
 }
 
 impl SlotMap {
     pub async fn new(config: &Config) -> eyre::Result<Arc<Mutex<Self>>> {
         let client = RpcClient::new(format!("{}/{}", config.rpc, config.rpc_key));
         client.get_health().await?;
-        let slots = Default::default();
+        let slots = LruCache::new(MAX_SLOTS);
         Ok(Arc::new(Mutex::new(Self { client, slots })))
     }
-    pub async fn get(&self, slot: &u64) -> eyre::Result<i64> {
+    pub async fn get(&mut self, slot: &u64) -> eyre::Result<i64> {
         for _ in 0..2 {
             match self.slots.get(slot).copied() {
                 None => {
@@ -32,6 +35,6 @@ impl SlotMap {
         Ok(time)
     }
     pub fn insert(&mut self, slot: u64, ts: i64) {
-        self.slots.insert(slot, ts);
+        self.slots.put(slot, ts);
     }
 }
